3190180e3ce6245121c907a66cbfa70b1e8615f0
Innovated-Studios <156177036+Innovated-Studios@users.noreply.github.com>
2024-01-10 08:33:14 +0000

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+use_experimental_fxv2_oal 'yes'
+lua54 'yes'
+games { 'gta5' }
+
+author 'Tracka'
+description 'Place a prop anywhere'
+version '1.0.0'
+
+
+client_scripts{
+    'client/client.lua'
+}
